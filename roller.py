85d230abf3e9f38be2748d2d6a7602ea2eda5448
Eviskull <Eviskull@users.noreply.github.com>
2021-04-15 22:27:41 +0100

    Update roller.py

--- roller.py
+++ roller.py
@@ -1,21 +1,21 @@
 import random   # Used for dice rolls
-import sys
+import sys # For running with arguments
 """
- Returns a random number between 0 and 5, inclusive. It's not 1 to 6 because I
- only care about how many of each number are rolled. So these just generate the
- indices of a list, rather than the actual dice value
- Added this function for readability. Plus, typing roll_d6 is just easier than
- random.randint
+Returns a random number between 0 and 5, inclusive. It's not 1 to 6 because I
+only care about how many of each number are rolled. So these just generate the
+indices of a list, rather than the actual dice value
+Added this function for readability. Plus, typing roll_d6 is just easier than
+random.randint
  
- Refactored after finding it on github and needing to read some code I didn't write to make sense of it.
+Refactored after finding it on github and needing to read some code I didn't write to make sense of it.
+Requires Python3.6 or later as it uses f"string" formatting
 """
 def roll_d6():
     return random.randint(0,5)
 
 def roll_dice(num):
-
-    # Each index holds the number of dice with each value.
-    # E.g. 0 -> 1s, 1 -> 2s, etc.
+    """ Each index holds the number of dice with each value.
+     E.g. 0 -> 1s, 1 -> 2s, etc."""
     rolls = [0, 0, 0, 0, 0, 0]
 
     # Rolls the specified number of dice, adding 1 to a random index each time
@@ -25,17 +25,18 @@ def roll_dice(num):
     
     
 def display_rolls(roll_list, num_dice):
-    #Refactored this to one line and added functionality, now shows
-    #{D6 Side} s|+ -- {rolls}/{uprolls} = {% of total rolls to 2 decimal places}
+    """Refactored this to one line and added functionality, now shows
+    {D6 Side} s|+ -- {rolls}/{uprolls} = {% of total rolls to 2 decimal places}"""
     for entry in range(0, len(roll_list)):
         print(f"{entry+1}s|+ -- {roll_list[entry]}|{sum(roll_list[entry:])} = {(roll_list[entry]/num_dice)*100:2.2f}%")
         
 def get_rollnumber():
-    #Break up inputs for debugging
+    """Break up inputs for debugging"""
     return int(input('Enter number of dice to throw?'))
 
 
 def main_loop():
+ """Main loop for menu"""
     while True:
         print('\nHello and welcome to the rolling tool\nEnter 1 to roll\nEnter Q to exit\n')
         user_choice = input('Enter your selection: ')
